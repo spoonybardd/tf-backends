bd6a9fd9e289b24b55cbc96987dd3512dd9d0531
Brad Gravesen <bgravesen@gmail.com>
2025-06-19 14:15:24 -0700

    Uncomment test and prod backend modules in main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ module "dev_backend" {
   subscription_id = var.subscription_id
 }
 
-# module "test_backend" {
-#   source          = "./modules/backend"
-#   environment     = "test"
-#   subscription_id = var.subscription_id
-# }
+module "test_backend" {
+  source          = "./modules/backend"
+  environment     = "test"
+  subscription_id = var.subscription_id
+}
 
-# module "prod_backend" {
-#   source          = "./modules/backend"
-#   environment     = "prod"
-#   subscription_id = var.subscription_id
-# }
+module "prod_backend" {
+  source          = "./modules/backend"
+  environment     = "prod"
+  subscription_id = var.subscription_id
+}
